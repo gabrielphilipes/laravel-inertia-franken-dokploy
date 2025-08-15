41c180b800872ac23c49f3784a096f6b520ead73
Gabriel Philipe S <48845302+gabrielphilipes@users.noreply.github.com>
2025-08-15 10:17:31 -0300

    refactor: remover importações não utilizadas do arquivo app.js para melhorar a legibilidade e a manutenção do código

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,6 @@ import { createInertiaApp } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createApp, h } from "vue";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy";
-import route from "ziggy-js";
-import { Ziggy } from "./ziggy";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
